1e20edbb3bf85230c7fe32b34d1ada9c08d96367
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-02-13 23:55:30 -0500

    Added address inputs

--- src/components/PostAd/PostAd.js
+++ src/components/PostAd/PostAd.js
@@ -20,43 +20,58 @@ export default class PostAd extends React.Component{
                 <form>
                     <fieldset>
 
-                        <label htmlFor="register-ad-first-name">Living space type:</label>
+                        <label htlFor="post-ad-address">Address</label>
+                        <input id="post-ad-address" type="text"></input>
 
-                        <select id="register-ad-first-name" >
+                        <label htmlFor="post-ad-apartment">Apartment location: if applicable</label>
+                        <input id="post-ad-apartment" type="text" placeholder="E.g. A12"></input>
+
+                        <label htmlFor="post-ad-city">City:</label>
+                        <input id="post-ad-city" type="text"/>
+
+                        <label htmlFor="post-ad-state">State:</label>
+                        <input id="post-ad-state" type="text"></input>
+
+                        <label htmlFor="post-ad-zipcode">Zip code:</label>
+                        <input id="post-ad-zipcode" type="text"></input>
+
+                        <label htmlFor="register-ad-type">Living space type:</label>
+
+                        <select id="register-ad-type" >
                             <option value="Bedroom">Bed Room</option>
                             <option value="Apartment">Apartment</option>
                         </select>
 
-                        <label htmlFor="register-ad-last-name">Number of rooms:</label>
+                        <label htmlFor="register-ad-rooms">Number of rooms:</label>
 
-                        <select id="register-ad-last-name" type="text">
+                        <select id="register-ad-rooms" type="text">
                             <option value={1}>1</option>
                             <option value={2}>2</option>
                             <option value={3}>3</option>
                             <option value={3}>3</option>
                         </select>
 
-                        <label htmlFor="register-ad-email">Number of bathrooms:</label>
-                        <select id="register-ad-last-name" type="email">
+                        <label htmlFor="register-ad-bathrooms">Number of bathrooms:</label>
+                        <select id="register-ad-bathrooms" type="email">
                             <option value={1}>1</option>
                             <option value={2}>2</option>
                             <option value={3}>3</option>
                             <option value={4}>4</option>
                         </select>
 
-                        <label htmlFor="register-ad-mobile-number">Pets:</label>
-                        <select id="register-ad-mobile-number" type="text">
+                        <label htmlFor="register-ad-pets">Pets:</label>
+                        <select id="register-ad-pets" type="text">
                             <option value="No pets">No pets</option>
                             <option value="Dogs ok">Dogs ok</option>
                             <option value="Cats ok">Cats ok</option>
                             <option value="Dogs and Cats ok">Dogs and Cats ok</option>
                         </select>
 
-                        <label htmlFor="register-ad-password">Password</label>
-                        <input id="register-ad-password" type="range" defaultValue={1000} min={0} max={10000}></input>
+                        <label htmlFor="register-ad-price">Price</label>
+                        <input id="register-ad-price" type="range" defaultValue={1000} min={0} max={10000}></input>
 
-                        <label htmlFor="register-ad-confirm-password">Images</label>
-                        <input id="register-ad-confirm-password" type="file"></input>
+                        <label htmlFor="register-ad-images">Images</label>
+                        <input id="register-ad-images" type="file"></input>
 
                         <button type="submit">Post Ad</button>
                     </fieldset>
