ec77e27e4e277666ba05d668419532f07884aa72
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-02-14 23:37:12 -0500

    Added onChange event listeners

--- src/components/PostAd/PostAd.js
+++ src/components/PostAd/PostAd.js
@@ -4,14 +4,59 @@ export default class PostAd extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            first_name: "",
-            last_name: "",
-            email: "",
-            mobile_number: "",
-            password: "",
+            address: "",
+            apartment_location: "",
+            city: "",
+            state: "",
+            zip_code: "",
+            space_type: "",
+            room_amount: "",
+            bathroom_amount: "",
+            pets: "",
+            price: "",
             error: ""
         };
     };
+
+    handleAddress = (e)=>{
+        this.setState({ address: e.target.value});
+    }
+
+    handleApartmentLocation = (e)=>{
+        this.setState({ apartment_location: e.target.value});
+    }
+
+    handleCity = (e)=>{
+        this.setState({ city: e.target.value});
+    }
+
+    handleState = (e)=>{
+        this.setState({ state: e.target.value});
+    }
+
+    handleZipcode = (e)=>{
+        this.setState({ zip_code: e.target.value});
+    }
+
+    handleSpaceType = (e)=>{
+        this.setState({ space_type: e.target.value});
+    }
+
+    handleRoomAmount = (e)=>{
+        this.setState({ room_amount: e.target.value});
+    }
+
+    handleBathroomAmount = (e)=>{
+        this.setState({ bathroom_amount: e.target.value});
+    }
+
+    handlePets = (e)=>{
+        this.setState({ pets: e.target.value});
+    }
+
+    handlePrice = (e)=>{
+        this.setState({ price: e.target.value});
+    }
 
     render(){
 
@@ -21,30 +66,56 @@ export default class PostAd extends React.Component{
                     <fieldset>
 
                         <label htlFor="post-ad-address">Address</label>
-                        <input id="post-ad-address" type="text"></input>
+                        <input 
+                            id="post-ad-address" 
+                            type="text"
+                            value={this.state.address} 
+                            onChange={this.handleAddress}></input>
 
                         <label htmlFor="post-ad-apartment">Apartment location: if applicable</label>
-                        <input id="post-ad-apartment" type="text" placeholder="E.g. A12"></input>
+                        <input 
+                            id="post-ad-apartment" 
+                            type="text" 
+                            placeholder="E.g. A12"
+                            value={this.state.apartment_location} 
+                            onChange={this.handleApartmentLocation}></input>
 
                         <label htmlFor="post-ad-city">City:</label>
-                        <input id="post-ad-city" type="text"/>
+                        <input 
+                            id="post-ad-city" 
+                            type="text"
+                            value={this.state.city} 
+                            onChange={this.handleCity}/>
 
                         <label htmlFor="post-ad-state">State:</label>
-                        <input id="post-ad-state" type="text"></input>
+                        <input 
+                            id="post-ad-state" 
+                            type="text"
+                            value={this.state.state} 
+                            onChange={this.handleState}></input>
 
                         <label htmlFor="post-ad-zipcode">Zip code:</label>
-                        <input id="post-ad-zipcode" type="text"></input>
+                        <input 
+                            id="post-ad-zipcode" 
+                            type="text"
+                            value={this.state.zip_code} 
+                            onChange={this.handleZipcode}></input>
 
                         <label htmlFor="register-ad-type">Living space type:</label>
 
-                        <select id="register-ad-type" >
+                        <select 
+                            id="register-ad-type" 
+                            value={this.state.space_type} 
+                            onChange={this.handleSpaceType}>
                             <option value="Bedroom">Bed Room</option>
                             <option value="Apartment">Apartment</option>
                         </select>
 
                         <label htmlFor="register-ad-rooms">Number of rooms:</label>
-
-                        <select id="register-ad-rooms" type="text">
+                        <select 
+                            id="register-ad-rooms" 
+                            value={this.state.room_amount} 
+                            onChange={this.handleRoomAmount}>
                             <option value={1}>1</option>
                             <option value={2}>2</option>
                             <option value={3}>3</option>
@@ -52,7 +123,10 @@ export default class PostAd extends React.Component{
                         </select>
 
                         <label htmlFor="register-ad-bathrooms">Number of bathrooms:</label>
-                        <select id="register-ad-bathrooms" type="email">
+                        <select 
+                            id="register-ad-bathrooms" 
+                            value={this.state.bathroom_amount} 
+                            onChange={this.handleBathroomAmount}>
                             <option value={1}>1</option>
                             <option value={2}>2</option>
                             <option value={3}>3</option>
@@ -60,7 +134,10 @@ export default class PostAd extends React.Component{
                         </select>
 
                         <label htmlFor="register-ad-pets">Pets:</label>
-                        <select id="register-ad-pets" type="text">
+                        <select 
+                            id="register-ad-pets" 
+                            value={this.state.pets} 
+                            onChange={this.handlePets}>
                             <option value="No pets">No pets</option>
                             <option value="Dogs ok">Dogs ok</option>
                             <option value="Cats ok">Cats ok</option>
@@ -68,10 +145,19 @@ export default class PostAd extends React.Component{
                         </select>
 
                         <label htmlFor="register-ad-price">Price</label>
-                        <input id="register-ad-price" type="range" defaultValue={1000} min={0} max={10000}></input>
+                        <input 
+                            id="register-ad-price" 
+                            type="range" 
+                            defaultValue={1000} 
+                            min={0} 
+                            max={10000}
+                            value={this.state.price} 
+                            onChange={this.handlePrice}></input>
 
                         <label htmlFor="register-ad-images">Images</label>
-                        <input id="register-ad-images" type="file"></input>
+                        <input 
+                            id="register-ad-images" 
+                            type="file"></input>
 
                         <button type="submit">Post Ad</button>
                     </fieldset>
